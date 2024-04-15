5205387f02cbc8d1dad77d966f7680b3564437ea
Lesia <leskinkotik@gmail.com>
2024-04-15 16:24:36 +0200

    added recherche le premier

--- test_jour_4.py
+++ test_jour_4.py
@@ -1,8 +1,8 @@
 import requests # type: ignore
 from bs4 import BeautifulSoup # type: ignore
 
-url="https://www.meudon.fr/"
-
+url = 'https://www.meudon.fr/actualites/'
+#отправляем запрос на сайт чтоб посмотреть на ответ и на ошибки
 response = requests.get(url)
 
 content = response.content
@@ -12,12 +12,14 @@ soup = BeautifulSoup(content,'html.parser')
 
 #h2 = soup.find('h2')
 #print(h2)
+
 #Найти все теги <a> на странице и вывести их содержимое:
 #links = soup.find_all('a')
 #for link in links:
 #   print(link.text)
+
 # Для поиска рисунка
-div = soup.find('div', {"class": "event-image"})
+"""div = soup.find('div', {"class": "event-image"})
 if div:
     image = div.find('img')
     if image:
@@ -26,5 +28,10 @@ if div:
     else:
         print('pas trouve')
 else:
-    print('div pas trouve')
+    print('div pas trouve')"""
+
+#recherche le premier 
+div = soup.find_all('div', {"class": "entry-content"})
+print(div[0])
+
 
