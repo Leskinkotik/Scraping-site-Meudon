b21b0395583078d7165be1fa26f5ee081d5ccfe6
Lesia <leskinkotik@gmail.com>
2024-04-15 12:54:38 +0200

    updated scrap_4

---
+++ test_jour_4.py
@@ -0,0 +1,30 @@
+import requests # type: ignore
+from bs4 import BeautifulSoup # type: ignore
+
+url="https://www.meudon.fr/"
+
+response = requests.get(url)
+
+content = response.content
+#print(content)
+
+soup = BeautifulSoup(content,'html.parser')
+
+#h2 = soup.find('h2')
+#print(h2)
+#Найти все теги <a> на странице и вывести их содержимое:
+#links = soup.find_all('a')
+#for link in links:
+#   print(link.text)
+# Для поиска рисунка
+div = soup.find('div', {"class": "event-image"})
+if div:
+    image = div.find('img')
+    if image:
+        img_source = image.get('src')
+        print(img_source)
+    else:
+        print('pas trouve')
+else:
+    print('div pas trouve')
+
