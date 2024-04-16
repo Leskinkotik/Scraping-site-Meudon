6789855b5083a420cc152cb5ebd7772b841bfc41
Lesia <leskinkotik@gmail.com>
2024-04-16 12:10:42 +0200

    updates

--- test_jour_4.py
+++ test_jour_4.py
@@ -1,29 +1,24 @@
-import requests # type: ignore
-from bs4 import BeautifulSoup # type: ignore
-
+import requests 
+from bs4 import BeautifulSoup 
 url = 'https://www.meudon.fr/actualites/'
 #отправляем запрос на сайт чтоб посмотреть на ответ и на ошибки
 response = requests.get(url)
-
 content = response.content
 #print(content)
-
 soup = BeautifulSoup(content,'html.parser')
 
-#h2 = soup.find('h2')
-#print(h2)
+# h2 = soup.find_all('h2')
+# print(h2)
 
-#Найти все ссылки <a> на странице и вывести их содержимое:
+# Найти все ссылки <a> на странице и вывести их содержимое:
 #links = soup.find_all('a')
 #for link in links:
-#    print(link)
+    #print(link)
     #print(link.text)
     #print(link.get('href'))
-    #print(link.text+link.get('href'))
+#    print(link.text+link.get('href'))
 
-
-
-# Для поиска рисунка
+# Cherche une image
 """div = soup.find('div', {"class": "event-image"})
 if div:
     image = div.find('img')
@@ -35,11 +30,10 @@ if div:
 else:
     print('div pas trouve')"""
 
-#recherche le premier 
-#div = soup.find_all('div', {"class": "entry-content"})
-#print(div[0])
+# cherche le premier 
+div = soup.find_all('div', {"class": "entry-content"})
+print(div[0])
 
-#.div = soup.
 
 arts = soup.find_all('article', {"class": "post"})
 if arts:
@@ -47,4 +41,4 @@ if arts:
         header = article.find('h3').text
         content = article.find('div', {"class": "entry-content"}).text
         link = article.find('a', {"class": "post-link"}).get('href')
-        print(header + ": " + content + " => " +  link)
+        print(header + " : " + content + " => " +  link)
